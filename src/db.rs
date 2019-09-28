a44c17a685f40ed324693a028c5eda21b4620a14
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-09-28 18:22:06 -0400

    Migrated to PSQL.
    Heroku does not support SQLite :^(

--- src/db.rs
+++ src/db.rs
@@ -2,12 +2,11 @@ use actix_web::{web, Error as AWError};
 use failure::Error;
 use futures::Future;
 use r2d2;
-use r2d2_sqlite;
-use rusqlite::NO_PARAMS;
+use r2d2_postgres;
 use serde_derive::{Deserialize, Serialize};
 
-pub type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
-pub type Connection = r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager>;
+pub type Pool = r2d2::Pool<r2d2_postgres::PostgresConnectionManager>;
+pub type Connection = r2d2::PooledConnection<r2d2_postgres::PostgresConnectionManager>;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Entry {
@@ -24,13 +23,21 @@ pub fn execute(pool: &Pool) -> impl Future<Item = Vec<Entry>, Error = AWError> {
 fn get_random_word(conn: Connection) -> Result<Vec<Entry>, Error> {
     let stmt = "SELECT * FROM entries ORDER BY RANDOM() LIMIT 1;";
 
-    let mut prep_stmt = conn.prepare(stmt)?;
-    let annuals = prep_stmt
-        .query_map(NO_PARAMS, |row| Entry {
+    let prep_stmt = conn.prepare(stmt).unwrap();
+    for row in &prep_stmt.query(&[]).unwrap() {
+        let a = Entry {
             word: row.get(0),
             wordtype: row.get(1),
             definition: row.get(2),
-        })
-        .and_then(|mapped_rows| Ok(mapped_rows.map(|row| row.unwrap()).collect::<Vec<Entry>>()))?;
-    Ok(annuals)
+        };
+        let b = vec![a];
+        return Ok(b);
+    }
+    let a = Entry {
+        word: "Please".to_string(),
+        wordtype: "Help".to_string(),
+        definition: "Me".to_string(),
+    };
+    let b = vec![a];
+    return Ok(b);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::io;
 
 use actix_web::{middleware, web, App, Error as AWError, HttpResponse, HttpServer};
 use futures::future::Future;
-use r2d2_sqlite;
-use r2d2_sqlite::SqliteConnectionManager;
+use r2d2_postgres;
 
 mod db;
 use db::Pool;
@@ -16,11 +15,24 @@ fn index(db: web::Data<Pool>) -> impl Future<Item = HttpResponse, Error = AWErro
 
 fn main() -> io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
+
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
+    let database_url: String = std::env::var("HEROKU_POSTGRESQL_CYAN_URL")
+        .unwrap_or_else(|_| "localhost:4532".to_string())
+        .parse()
+        .expect("Invalid database URL");
+
     env_logger::init();
     let sys = actix_rt::System::new("rustom_word");
 
     // Start N db executor actors (N = number of cores avail)
-    let manager = SqliteConnectionManager::file("dictionary.db");
+    let manager =
+        r2d2_postgres::PostgresConnectionManager::new(database_url, r2d2_postgres::TlsMode::None)
+            .unwrap();
     let pool = Pool::new(manager).unwrap();
 
     // Start http server
@@ -30,9 +42,7 @@ fn main() -> io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(web::resource("/").route(web::get().to_async(index)))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("0.0.0.0", port))?
     .start();
-
-    println!("Started http server: 127.0.0.1:8080");
     sys.run()
 }
