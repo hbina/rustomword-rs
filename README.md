18090bf8867ffe2bb0ecf32425f9f0a4f3cca679
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-09-29 14:18:14 -0400

    Made setting up server easier.
    1. Updated README.md to include instructions
    2. Updated main() to consider local setup as a fallback.

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # rustomword
+
 A play on random word, gives you a random word that you can use for ... whatever words are for.
 
 For now, there is no real way to set it up locally.
 You must deploy it to Heroku.
 A live one is available here : https://rustomword.herokuapp.com/
+
+# Setting Up Local Instance
+
+## Notes
+
+Since Heroku's database URI are dynamic, there is no way to test local instance of this application against the `rustomword`'s backend. Hence, you will have to provide the database yourself.
+
+Thankfully, it's not *that* hard.
+
+## Prerequisite
+
+Currently, our setup is not database agnostic.
+You will have to install PostgreSQL.
+
+## Instructions
+
+1. Access the database, and create a user using the name `rustomword_test` and the password `654321`. This is just for local testing, so I thought its fine to expose them here :3
+2. Create a database with the name `rustomword_test`.
+3. Clone this repository : https://github.com/hbina/english_dict_sql
+4. You will then have to feed the database with the `entries` table which is provided by the SQL file. To do so, simply execute : `psql -d rustomword < psql_entries.sql`
+5. Execute `cargo run`. You will encounter "errors" saying some environment variables are not set up, but you can ignore them.
--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,23 @@ fn main() -> io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
 
     let port: u16 = std::env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string())
+        .unwrap_or_else(|err| {
+            println!("parsing $PORT returns err:{} defaulting to 3000", err);
+            "3000".to_string()
+        })
         .parse()
         .expect("PORT must be a number");
 
     let database_url: String = std::env::var("HEROKU_POSTGRESQL_CYAN_URL")
-        .unwrap_or_else(|_| "localhost:4532".to_string())
+        .unwrap_or_else(|err| {
+            println!(
+                "parsing $HEROKU_POSTGRESQL_CYAN_URL returns err:{} defaulting to localhost:4532",
+                err
+            );
+            "postgres://rustomword_test:654321@localhost:5432/rustomword_test".to_string()
+        })
         .parse()
-        .expect("Invalid database URL");
+        .expect("Invalid database URI");
 
     env_logger::init();
     let sys = actix_rt::System::new("rustom_word");
@@ -32,17 +41,26 @@ fn main() -> io::Result<()> {
     // Start N db executor actors (N = number of cores avail)
     let manager =
         r2d2_postgres::PostgresConnectionManager::new(database_url, r2d2_postgres::TlsMode::None)
+            .map_err(|error| println!("unable to connect to error:{}", error))
             .unwrap();
     let pool = Pool::new(manager).unwrap();
 
     // Start http server
-    HttpServer::new(move || {
+    match HttpServer::new(move || {
         App::new()
             .data(pool.clone())
             .wrap(middleware::Logger::default())
             .service(web::resource("/").route(web::get().to_async(index)))
     })
-    .bind(("0.0.0.0", port))?
-    .start();
+    .bind(("0.0.0.0", port))
+    {
+        Ok(ok) => {
+            println!("launching server"); // TODO :: Print current time
+            ok.start();
+        }
+        Err(err) => {
+            panic!("unable to bind:{}", err);
+        }
+    };
     sys.run()
 }
